689ce35ee4497866a63ef4acbf31ae21ea1aca61
Diogo Meneses(真龍)☸️ <henriquediogo434@gmail.com>
2024-04-09 11:41:52 -0300

    Pills components.

---
+++ most-used-words/src/components/HomePage.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="pills">
+        <PillComponent v-for="item in groupedwords" :word="item.word" :amount="item.amount" :key="item.word" />
+    </div>
+</template>
+
+<script>
+import PillComponent from './PillComponent.vue'
+
+export default {
+    components: {
+        PillComponent
+    },
+    data: function () {
+        return {
+            groupedwords: [
+                { word: "i", amount: 547 },
+                { word: "you", amount: 47 },
+                { word: "it", amount: 10 }
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+.pills{
+    display: flex;
+}
+
+</style>
---
+++ most-used-words/src/components/PillComponent.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="pill">
+        <span class="word">{{ word }}</span>
+        <span class="amount">{{ amount }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    props:["word", "amount"]
+}
+</script>
+
+<style>
+
+.pill{
+    margin: 10px;
+    height: 55px;
+    width: fit-content;
+    border-radius: 35px;
+    color: #ffff;
+    font-size: 1.3rem;
+    font-weight: bold;
+    display: flex;
+    align-items: enter;
+    background-color: red;
+}
+
+.pill .word{
+    margin-left: 15px;
+}
+
+.pill .amount{
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    background-color: #ffff;
+    color: red;
+    margin: 10px;
+    padding: 5px;
+    font-size: 14px;
+    display: flex;
+    justify-content: center;
+    align-content: center;
+}
+</style>
