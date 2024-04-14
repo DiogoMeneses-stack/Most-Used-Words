796fccacedcf29ee2f9aadc6625bf3d9389f7e07
Diogo Meneses(真龍)☸️ <henriquediogo434@gmail.com>
2024-04-13 22:22:21 -0300

    Send.

--- most-used-words/src/components/HomePage.vue
+++ most-used-words/src/components/HomePage.vue
@@ -1,4 +1,17 @@
 <template>
+    <v-content fluid>
+        <v-form>
+            <v-file-input
+            label="Selecione as legendas"
+            prepend-icon="mdi-message-text"
+            append-icon="mdi-send"
+            outlined
+            multiple
+            v-model="files"
+            @click:append="processSubtitles"
+            >
+            </v-file-input>
+        </v-form>
     <div class="pills">
         <PillComponent v-for="item in groupedwords" :word="item.word" :amount="item.amount" :key="item.word" />
     </div>
@@ -13,11 +26,17 @@ export default {
     },
     data: function () {
         return {
+            files:[],
             groupedwords: [
                 { word: "i", amount: 547 },
                 { word: "you", amount: 47 },
                 { word: "it", amount: 10 }
             ]
+        }
+    },
+    methods:{
+        processSubtitles(){
+            console.log(this.files);
         }
     }
 }
