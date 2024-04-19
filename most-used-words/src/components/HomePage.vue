2445973350cf92eccc908729874cce77d0b6a4fe
Diogo Meneses(真龍)☸️ <henriquediogo434@gmail.com>
2024-04-19 13:49:26 -0300

    Pills Center.

--- most-used-words/src/components/HomePage.vue
+++ most-used-words/src/components/HomePage.vue
@@ -1,19 +1,13 @@
 <template>
-    <v-content fluid>
+    <div class="home">
         <v-form>
-            <v-file-input
-            label="Selecione as legendas"
-            prepend-icon="mdi-message-text"
-            append-icon="mdi-send"
-            outlined
-            multiple
-            v-model="files"
-            @click:append="processSubtitles"
-            >
+            <v-file-input label="Selecione as legendas" prepend-icon="mdi-message-text" append-icon="mdi-send" outlined
+                multiple v-model="files" @click:append="processSubtitles">
             </v-file-input>
         </v-form>
-    <div class="pills">
-        <PillComponent v-for="item in groupedwords" :word="item.word" :amount="item.amount" :key="item.word" />
+        <div class="pills">
+            <PillComponent v-for="item in groupedwords" :word="item.word" :amount="item.amount" :key="item.word" />
+        </div>
     </div>
 </template>
 
@@ -26,7 +20,7 @@ export default {
     },
     data: function () {
         return {
-            files:[],
+            files: [],
             groupedwords: [
                 { word: "i", amount: 547 },
                 { word: "you", amount: 47 },
@@ -34,8 +28,8 @@ export default {
             ]
         }
     },
-    methods:{
-        processSubtitles(){
+    methods: {
+        processSubtitles() {
             console.log(this.files);
         }
     }
@@ -43,8 +37,13 @@ export default {
 </script>
 
 <style scoped>
-.pills{
-    display: flex;
+.home {
+    margin: 20px;
 }
 
+.pills {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+}
 </style>
